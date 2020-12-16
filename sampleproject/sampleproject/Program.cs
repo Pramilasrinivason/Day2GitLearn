1539300c14927f74025e9215b127ec1b11d2ac11
Pramilasrinivason <75603850+Pramilasrinivason@users.noreply.github.com>
2020-12-16 11:11:17 +0530

    Update Program.cs

--- sampleproject/sampleproject/Program.cs
+++ sampleproject/sampleproject/Program.cs
@@ -10,7 +10,7 @@ namespace sampleproject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
